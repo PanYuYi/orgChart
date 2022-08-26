fc0587011b5bd43e7ec5b0950afe8bb8097f5a97
潘裕艺 <panyuyi811@163.com>
2022-08-26 17:31:35 +0800

    删除垃圾代码

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import canvasDemo from "./components/canvasDemo.vue";
 import topologyDemo from "./components/topologyDemo.vue";
 </script>
 
 <template>
-  <!-- <canvasDemo /> -->
   <topologyDemo></topologyDemo>
 </template>
 
